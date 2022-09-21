cd8f246c6d5602c473b36723479d2cc74d0d32c8
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-20 23:01:57 -0500

    Revert "Added api route to get all posts"
    This reverts commit 3b67308abf2fb273b2e69a1af72cc7db52101148.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const threadRoutes = require('./threadRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', threadRoutes);
 
 module.exports = router;
