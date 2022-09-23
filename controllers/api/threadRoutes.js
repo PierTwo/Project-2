5f751e66c5dbe5c94245c513795b7c22fc415e0f
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-22 22:03:18 -0500

    Fixed unused variable

--- controllers/api/threadRoutes.js
+++ controllers/api/threadRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
 const { User, Category, Thread, Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
